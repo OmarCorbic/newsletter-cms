31d4e0c6adf2daf8161e1527a74833d1310c029b
Omar Corbic <omar_corbic@hotmail.com>
2024-06-20 11:26:39 +0200

    Added code editor component

---
+++ src/app/ui/templates/code-editor.tsx
@@ -0,0 +1,26 @@
+import { Editor } from "@monaco-editor/react";
+
+function CodeEditor({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (value?: string) => void;
+}) {
+  return (
+    <Editor
+      defaultValue={value.trim()}
+      language="html"
+      theme="vs-dark"
+      onChange={onChange}
+      options={{
+        autoClosingQuotes: "always",
+        fontSize: 14,
+        minimap: { enabled: false },
+        contextmenu: false,
+      }}
+    />
+  );
+}
+
+export default CodeEditor;
