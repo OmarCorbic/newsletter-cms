0ba058ffc470c0168f671bc50953284080b32711
Omar Corbic <omar_corbic@hotmail.com>
2024-07-01 13:05:39 +0200

    Wrapped fetching in try catch blocks

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -13,85 +13,115 @@ export async function fetchFilteredCustomers(
   let results: any = [];
   const searchPattern = `%${search}%`;
 
-  if (!groupId) {
-    results = await query(
-      "SELECT * FROM customers WHERE LOWER(customers.name) LIKE LOWER(?) OR LOWER(customers.email) LIKE LOWER(?)",
-      [searchPattern, searchPattern]
-    );
-  } else {
-    results = await query(
-      "SELECT c.id, c.name, c.email, c.company FROM grouped_customers gc JOIN customers c ON gc.customer_id = c.id WHERE  gc.group_id = ? AND (LOWER(c.name) LIKE LOWER(?) OR LOWER(c.email) LIKE LOWER(?))",
-      [groupId, searchPattern, searchPattern]
-    );
-  }
+  try {
+    if (!groupId) {
+      results = await query(
+        "SELECT * FROM customers WHERE LOWER(customers.name) LIKE LOWER(?) OR LOWER(customers.email) LIKE LOWER(?)",
+        [searchPattern, searchPattern]
+      );
+    } else {
+      results = await query(
+        "SELECT c.id, c.name, c.email, c.company FROM grouped_customers gc JOIN customers c ON gc.customer_id = c.id WHERE  gc.group_id = ? AND (LOWER(c.name) LIKE LOWER(?) OR LOWER(c.email) LIKE LOWER(?))",
+        [groupId, searchPattern, searchPattern]
+      );
+    }
 
-  const customers: Customer[] = [...results];
-  return customers;
+    const customers: Customer[] = [...results];
+    return customers;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch filtered customers");
+  }
 }
 export async function fetchGroupedCustomers(): Promise<Customer[]> {
   noStore();
-  const session = await getSession();
-  const userId = session?.user?.id || "";
-  const results: any = await query(
-    "SELECT * FROM grouped_customers JOIN CUSTOMERS",
-    [userId]
-  );
+  try {
+    const session = await getSession();
+    const userId = session?.user?.id || "";
+    const results: any = await query(
+      "SELECT * FROM grouped_customers JOIN CUSTOMERS",
+      [userId]
+    );
 
-  const customers: Customer[] = [...results];
-  return customers;
+    const customers: Customer[] = [...results];
+    return customers;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch grouped customers");
+  }
 }
 
 export async function fetchUserGroups(): Promise<Group[]> {
   noStore();
-  const session = await getSession();
-  const userId = session?.user?.id || "";
+  try {
+    const session = await getSession();
+    const userId = session?.user?.id || "";
 
-  const results: any = await query(
-    "SELECT * FROM user_groups WHERE user_id=?",
-    [userId]
-  );
+    const results: any = await query(
+      "SELECT * FROM user_groups WHERE user_id=?",
+      [userId]
+    );
 
-  const groups: Group[] = [...results];
-  return groups;
+    const groups: Group[] = [...results];
+    return groups;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch user groups");
+  }
 }
 
 export async function fetchTemplates(): Promise<Template[]> {
   noStore();
-  const session = await getSession();
-  const userId = session?.user?.id || "";
-  const results: any = await query("SELECT * FROM templates WHERE user_id=?", [
-    userId,
-  ]);
+  try {
+    const session = await getSession();
+    const userId = session?.user?.id || "";
+    const results: any = await query(
+      "SELECT * FROM templates WHERE user_id=?",
+      [userId]
+    );
 
-  const templates: Template[] = results?.map((template: any) => ({
-    ...template,
-    inputs: JSON.parse(template.inputs),
-  }));
-  return templates;
+    const templates: Template[] = results?.map((template: any) => ({
+      ...template,
+      inputs: JSON.parse(template.inputs),
+    }));
+    return templates;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch templates");
+  }
 }
 
-export async function fetchSingleTemplateHTML(
-  templateId: string
-): Promise<{ html: string; inputs: Input[] }> {
+export async function fetchTemplateById(templateId: string): Promise<Template> {
   noStore();
-  const results: any = await query("SELECT * FROM templates WHERE id=?", [
-    templateId,
-  ]);
+  try {
+    const results: any = await query("SELECT * FROM templates WHERE id=?", [
+      templateId,
+    ]);
 
-  if (results.length < 1) throw new Error("Template doesn't exist");
+    if (results.length < 1) throw new Error("Template doesn't exist");
 
-  const template = results[0];
-  return { html: template.html, inputs: JSON.parse(template.inputs) };
+    const template = { ...results[0], inputs: JSON.parse(results[0].inputs) };
+    return template;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch template");
+  }
 }
 
 export async function fetchPresets(): Promise<Preset[]> {
   noStore();
-  const session = await getSession();
-  const userId = session?.user?.id || "";
-  const results: any = await query("SELECT * FROM presets WHERE user_id=?", [
-    userId,
-  ]);
 
-  const presets: Preset[] = [...results];
-  return presets;
+  try {
+    const session = await getSession();
+    const userId = session?.user?.id || "";
+    const results: any = await query("SELECT * FROM presets WHERE user_id=?", [
+      userId,
+    ]);
+
+    const presets: Preset[] = [...results];
+    return presets;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error("Database Error: Failed to fetch presets");
+  }
 }
