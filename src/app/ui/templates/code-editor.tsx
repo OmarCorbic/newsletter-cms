97f9a8feb3b4b857fd91f9e903feb5cf75c20695
Omar Corbic <omar_corbic@hotmail.com>
2024-07-25 13:42:59 +0200

    Added editor code formating options

--- src/app/ui/templates/code-editor.tsx
+++ src/app/ui/templates/code-editor.tsx
@@ -1,4 +1,12 @@
+import React, { useRef } from "react";
+
 import { Editor } from "@monaco-editor/react";
+
+const trimNewlinesInStyleAttributes = (html: string) => {
+  const replaceNewlines = (match: string) => match.replace(/\n\s*/g, " ");
+  const pattern = /style="[^"]*"/g;
+  return html.replace(pattern, replaceNewlines);
+};
 
 function CodeEditor({
   value,
@@ -7,20 +15,41 @@ function CodeEditor({
   value: string;
   onChange: (value?: string) => void;
 }) {
+  const editorRef = useRef<any>(null);
+  const handleKeyDown = (e: any) => {
+    if (e.ctrlKey && e.key.toLowerCase() === "s") {
+      e.preventDefault();
+      const brokenText: string = editorRef.current.getValue();
+      const fixedText = trimNewlinesInStyleAttributes(brokenText);
+      editorRef.current.setValue(fixedText);
+    }
+  };
+
+  const handleEditorDidMount = (editor: any, monaco: any) => {
+    editorRef.current = editor;
+  };
+
   return (
-    <Editor
-      defaultValue={value.trim()}
-      language="html"
-      theme="vs-dark"
-      onChange={onChange}
-      options={{
-        autoClosingQuotes: "always",
-        fontSize: 14,
-        trimAutoWhitespace: true,
-        minimap: { enabled: false },
-        contextmenu: false,
-      }}
-    />
+    <div onKeyDown={handleKeyDown} className="h-full">
+      <Editor
+        onMount={handleEditorDidMount}
+        defaultValue={value.trim()}
+        language="html"
+        theme="vs-dark"
+        onChange={onChange}
+        options={{
+          padding: { top: 20, bottom: 20 },
+          tabSize: 2,
+          formatOnPaste: true,
+          wordWrap: "on",
+          detectIndentation: false,
+          autoClosingQuotes: "always",
+          fontSize: 14,
+          minimap: { enabled: false },
+          contextmenu: false,
+        }}
+      />
+    </div>
   );
 }
 
