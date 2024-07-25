7ea552614b5b94681629352f26b6cdbba4aaea34
Omar Corbic <omar_corbic@hotmail.com>
2024-07-25 13:38:07 +0200

    Renamed or moved

---
+++ src/app/ui/send-newsletter/choose-template.tsx
@@ -0,0 +1,58 @@
+"use client";
+import clsx from "clsx";
+import { useState } from "react";
+import TemplateForm from "./template-form";
+import PresetForm from "./preset-form";
+import { Customer, Preset, Template } from "@/app/lib/definitions";
+
+export default function ChooseTemplate({
+  customers,
+  templates,
+  presets,
+}: {
+  customers: Customer[];
+  templates: Template[];
+  presets: Preset[];
+}) {
+  const [fromTemplate, setFromTemplate] = useState(true);
+
+  return (
+    <div>
+      <h2 className="text-xl font-bold text-white py-4">Template</h2>
+      <div className="flex gap-2">
+        <button
+          onClick={() => setFromTemplate(true)}
+          className={clsx(
+            "flex w-32 items-center justify-center rounded-md  py-2 px-3 text-sm font-medium hover:bg-blue-900 hover:text-white text-white ",
+            {
+              "bg-blue-500 ": fromTemplate,
+              "bg-slate-900 ": !fromTemplate,
+            }
+          )}
+        >
+          From template
+        </button>
+        <button
+          onClick={() => setFromTemplate(false)}
+          className={clsx(
+            "flex w-32 items-center justify-center rounded-md py-2 px-3 text-sm font-medium hover:bg-blue-900 hover:text-white text-white ",
+            {
+              "bg-blue-500": !fromTemplate,
+              "bg-slate-900": fromTemplate,
+            }
+          )}
+        >
+          From preset
+        </button>
+      </div>
+
+      <div className="flex flex-row py-6 gap-3 h-screen">
+        {fromTemplate ? (
+          <TemplateForm templates={templates} customers={customers} />
+        ) : (
+          <PresetForm presets={presets} customers={customers} />
+        )}
+      </div>
+    </div>
+  );
+}
